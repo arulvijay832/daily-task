ec3cc33f85f20ca24d2940c6723b574b8094ce7c
arulvijay832 <144763624+arulvijay832@users.noreply.github.com>
2025-07-15 21:09:26 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,10 @@
+a=250;
+b=12;
+sum=a+b;
+sub=a-b;
+multi=a*b;
+div=a/b;
+console.log("Addition: "+sum);
+console.log("Subraction: "+sub);
+console.log("Multi: "+multi);
+console.log("Division: "+div)
---
+++ divisible.js
@@ -0,0 +1,11 @@
+a=600;
+if(a%3==0 && a%5==0)
+
+    {
+        console.log("Given number is divisible by both 3,5");
+    }
+    else
+        {
+            console.log("Given number is not divisible by both 3,5");
+
+    }
